aa6668a0f213686239a07d20d7c1fe9eb97cfe6e
BhaveshG-22 <107833962+BhaveshG-22@users.noreply.github.com>
2023-03-06 11:11:22 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express')
-
 const app=express();
 const port=9000;
+const { connectToDb, getDb } = require('./db')
+const { ObjectId } = require('mongodb')
 
-app.listen(9000,()=>{
-    console.log("App runing on port 9000");
+// db connection
+let db
+connectToDb((err) => {
+    if (!err) {
+        app.listen(process.env.PORT || 9000, () => {
+        })
+        db = getDb()
+    }
 })
 
-app.get("/",(req,res)=>{
-    res.send("Working")
+// app.listen(9000,()=>{
+//     console.log("App runing on port 9000");
+// })
+
+// ejs setup
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+
+    let blogs = []
+    db.collection('blogs')
+        .find()
+        .sort({ _id: -1 })
+        .forEach(blog => blogs.push(blog))
+        .then(() => {
+
+            res.render('index', { articles: blogs })
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'could not get data' })
+        })
+
 })
